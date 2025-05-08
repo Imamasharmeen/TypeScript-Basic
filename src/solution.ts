fe0a682e5821d66f61fc659f15762852146c7763
Imamasharmeen <imamasharmeen@gmail.com>
2025-05-08 11:28:01 +0300

    Created filterByRating function to filter objects by rating

--- src/solution.ts
+++ src/solution.ts
@@ -1,7 +1,14 @@
-function formatString(input: string, toUpper?: boolean): string {
+function formatString(input: string, toUpper: boolean = true): string {
     if (toUpper) {
         return input.toUpperCase();
     } else {
         return input.toLowerCase();
     }
 }
+
+function filterByRating(items: 
+    { title: string; rating: number }[])
+    : { title: string; rating: number }[] {
+    return items.filter(item => item.rating >= 4);
+}
+
