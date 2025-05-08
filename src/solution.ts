65856d649149ac9a0a8a9f1324b89fdbb9bfde87
Imamasharmeen <imamasharmeen@gmail.com>
2025-05-08 11:46:43 +0300

    Vehicle and Car classes with private properties and methods

--- src/solution.ts
+++ src/solution.ts
@@ -12,3 +12,36 @@ function filterByRating(items:
     return items.filter(item => item.rating >= 4);
 }
 
+function concatenateArrays<T>(...arrays: T[][]): T[] {
+    return arrays.flat();
+}
+
+
+class Vehicle {
+    private make: string;
+    private year: number;
+
+    constructor(make: string, year: number) {
+        this.make = make;
+        this.year = year;
+    }
+
+    getInfo(): string {
+        return `Make: ${this.make}, Year: ${this.year}`;
+    }
+}
+
+class Car extends Vehicle {
+    private model: string;
+
+    constructor(make: string, year: number, model: string) {
+        super(make, year);
+        this.model = model;
+    }
+
+    getModel(): string {
+        return `Model: ${this.model}`;
+    }
+}
+
+
